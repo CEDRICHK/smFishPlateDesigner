7a3cdea4bff68d11642f7dfbd7173994ef534d48
CEDRICHK <cedric.hassen-khodja@mri.cnrs.fr>
2024-01-29 14:04:05 +0100

    add documentation

---
+++ doc/smFishPlateDesign.R
@@ -0,0 +1,10 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----run-targets, eval=FALSE--------------------------------------------------
+#  library(targets)
+#  tar_make()
+
