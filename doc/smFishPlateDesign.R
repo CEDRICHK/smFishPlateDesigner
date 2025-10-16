80d0310735586ebd05147fcf6a1deaa98a7850d4
CEDRICHK <cedric.hassen-khodja@mri.cnrs.fr>
2025-10-16 18:14:25 +0200

    vignettes and site pkgdown create

--- doc/smFishPlateDesign.R
+++ doc/smFishPlateDesign.R
@@ -5,6 +5,17 @@ knitr::opts_chunk$set(
 )
 
 ## ----run-targets, eval=FALSE--------------------------------------------------
-#  library(targets)
-#  tar_make()
+# # Before running the `targets` workflow, set the path to your Excel file:
+# options(smFishPlateDesigner_excel_path = "path/to/your/excel_file.xlsx")
+# 
+# library(targets)
+# tar_make()
+# 
+# # else No action required for the file path - uses the default embedded file
+# library(targets)
+# tar_make()
+
+## ----manual-export, eval=FALSE------------------------------------------------
+# plates <- getPCR("path/to/your/excel_file.xlsx")
+# write_plate_workbook(plates, "output/pcr_plate_layouts.xlsx")
 
